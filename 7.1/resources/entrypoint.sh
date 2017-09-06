e101372558e91c5369312aa48d487d589a2f8d86
Stephane BARON <sbaron@galilee.fr>
2017-09-06 12:14:36 +0200

    Silence the whisperers

--- 7.1/resources/entrypoint.sh
+++ 7.1/resources/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 
 if [ -n "$SSH_PRIVATE_KEY" ]; then
 	eval $(ssh-agent -s)
-	ssh-add <(echo "$SSH_PRIVATE_KEY")
+	ssh-add <(echo "$SSH_PRIVATE_KEY") >/dev/null 2>&1
 	unset SSH_PRIVATE_KEY
 fi
 
